94fffff73bf88b9bc3611683fa2d7c943f434e32
Enzo DAL  CERRO <edal--ce@e2r2p2.42.fr>
2019-12-30 06:21:16 +0100

    upd

--- src/init.sh
+++ src/init.sh
@@ -1,14 +1,6 @@
-chown -R root:www-data /var/www/
 service mysql start
 service php7.3-fpm start
 service nginx start
 mysql -u root -ptoor < /docker-entrypoint-initdb.d/test.sql
 chown -R www-data:www-data /var/www
-#sed -i '57s/#//' /etc/nginx/sites-available/default
-#sed -i '56s/#//' /etc/nginx/sites-available/default
-#sed -i '60s/#//' /etc/nginx/sites-available/default
-#sed -i '63s/#//' /etc/nginx/sites-available/default
-#sed -i '46s/_;/wordpress;/' /etc/nginx/sites-available/default
-#sed -i '57s/#//' /etc/nginx/sites-available/default
-#sed -i '44s/index.html/index.php/' /etc/nginx/sites-available/default
 bash
