9b785b19cecc1dfa56aeeaa97f082732b96094d8
Enzo DAL  CERRO <edal--ce@e2r2p2.42.fr>
2019-12-30 04:49:22 +0100

    phpadmin + wprss + db OK, subtilites exo now

--- src/test.sql
+++ src/test.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE wordpress;
-CREATE USER test@localhost IDENTIFIED BY "password";
-GRANT ALL PRIVILEGES ON wordpress.* TO test@localhost;
+CREATE USER username@localhost IDENTIFIED BY "password";
+GRANT ALL PRIVILEGES ON wordpress.* TO username@localhost;
 FLUSH PRIVILEGES;
---
+++ test.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE wordpress;
+CREATE USER test@localhost IDENTIFIED BY "password";
+GRANT ALL PRIVILEGES ON WORDPRESS.* TO test@localhost;
+FLUSH PRIVILEGES;
